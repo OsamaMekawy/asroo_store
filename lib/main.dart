ee0e186e9ef80c7d5848aedc25f9b2a949dd6c0f
laptop home <osama.mekawy@gmail.com>
2024-05-02 00:15:47 +0300

    firebase configuretion

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:asro/asroo_store_app.dart';
+import 'package:asro/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main()async {
+
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const AsrooStoreApp());
 }
 
