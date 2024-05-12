c2a2c258fb3927afeb50197f92af56d11f3be164
laptop home <osama.mekawy@gmail.com>
2024-05-12 14:03:22 +0300

    bloc, shared

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:asro/asroo_store_app.dart';
+import 'package:asro/core/app/bloc_observer.dart';
 import 'package:asro/core/app/env.variables.dart';
 import 'package:asro/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main()async {
 
@@ -12,6 +14,7 @@ await EnvVariables.instance.init(envTypes: EnvTypes.dev);
 await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
+Bloc.observer = AppBlocObserver();
 
 await SystemChrome.setPreferredOrientations(
   [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
