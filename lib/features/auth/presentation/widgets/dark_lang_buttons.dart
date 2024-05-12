7feeb5423e3f13e9dfdf87396a6ab62a1804c11f
laptop home <osama.mekawy@gmail.com>
2024-05-12 22:59:44 +0300

    app cubit and language and shared prefereces

--- lib/features/auth/presentation/widgets/dark_lang_buttons.dart
+++ lib/features/auth/presentation/widgets/dark_lang_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:asro/core/app/app_cubit/app_cubit.dart';
 import 'package:asro/core/common/animation/animation_do.dart';
 import 'package:asro/core/common/widgets/custem_leanier_button.dart';
 import 'package:asro/core/common/widgets/text_app.dart';
@@ -6,6 +7,7 @@ import 'package:asro/core/language/app_localization.dart';
 import 'package:asro/core/language/lang_keys.dart';
 import 'package:asro/core/style/fonts/font_widget_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DarkAndLangButtons extends StatelessWidget {
@@ -13,21 +15,34 @@ class DarkAndLangButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<AppCubit>();
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        CustomFadeInRight(
-          duration: 400,
-          child: CustomLinearButton(
-          onPressed: (){},
-         child: const Icon(Icons.light_mode_rounded,color: Colors.white,)
-         )
-         ),
-          CustomFadeInLeft(
+        BlocBuilder(
+          bloc: cubit,
+          builder: (context, state) {
+            return CustomFadeInRight(
+                duration: 400,
+                child: CustomLinearButton(
+                    onPressed:cubit.changeAppThemeMode,
+                    child:  Icon(
+                     cubit.isDark? Icons.light_mode_rounded :Icons.dark_mode_rounded,
+                      color: Colors.white,
+                    )));
+          },
+        ),
+        
+        // lang button
+        CustomFadeInLeft(
           duration: 400,
           child: CustomLinearButton(
             onPressed: () {
-            
+                if (AppLocalizations.of(context)!.isEnLocale) {
+                cubit.toArabic();
+              } else {
+                cubit.toEnglish();
+              }
             },
             height: 44.h,
             width: 100.w,
@@ -40,7 +55,7 @@ class DarkAndLangButtons extends StatelessWidget {
               ),
             ),
           ),
-          )
+        )
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:asro/asroo_store_app.dart';
 import 'package:asro/core/app/bloc_observer.dart';
 import 'package:asro/core/app/env.variables.dart';
+import 'package:asro/core/di/injection_container.dart';
+import 'package:asro/core/service/sharedPrefereces/shared_pref.dart';
 import 'package:asro/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main()async {
 
@@ -15,6 +18,8 @@ await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
 Bloc.observer = AppBlocObserver();
+await SharedPref().instantiatePreferences();
+await setupInjector();
 
 await SystemChrome.setPreferredOrientations(
   [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
